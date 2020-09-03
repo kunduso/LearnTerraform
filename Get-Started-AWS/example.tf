a32acd84bb8b3d8114265d749cde35f0ab7656be
Sourav Kundu <sourav12kundu@gmail.com>
2020-09-02 21:22:43 -0500

    adding a provisioner

--- Get-Started-AWS/example.tf
+++ Get-Started-AWS/example.tf
@@ -14,4 +14,8 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
