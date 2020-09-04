99d440641c90133fd11372a6be554b9e75c4b354
Sourav Kundu <sourav12kundu@gmail.com>
2020-09-04 06:57:17 -0500

    added provision to rdp into a windows ec2 instance with proper key pair

--- Get-Started-AWS/example.tf
+++ Get-Started-AWS/example.tf
@@ -14,8 +14,18 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = "t2.micro"
+  key_name = "terraform-key"
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
+}
 
-  provisioner "local-exec" {
-  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+resource "aws_security_group" "allow_rdp" {
+  name = "allow_rdp"
+  description = "allow traffic"
+
+  ingress {
+    from_port = 3389
+    to_port = 3389
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
