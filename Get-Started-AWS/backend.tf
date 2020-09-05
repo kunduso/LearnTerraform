8277de5bd0181786b62e8a21f99e148947978187
Sourav Kundu <sourav12kundu@gmail.com>
2020-09-05 06:33:54 -0500

    updated region

--- Get-Started-AWS/backend.tf
+++ Get-Started-AWS/backend.tf
@@ -2,6 +2,6 @@ terraform {
     backend "s3" {
         bucket = "terraform-gettingstarted-aws"
         key = "terraform.tfstate"
-        region = "us-east-1"
+        region = "us-east-2"
     }
 }
