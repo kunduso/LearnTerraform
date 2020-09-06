d7a85dc5bae7ac22d115dfe98de2c26e628850aa
Sourav Kundu <sourav12kundu@gmail.com>
2020-09-06 06:19:32 -0500

    separating resources into individual terraform files

--- Get-Started-AWS/example.tf
+++ Get-Started-AWS/example.tf
@@ -10,22 +10,3 @@ provider "aws" {
   profile = "default"
   region  = var.region
 }
-
-resource "aws_instance" "example" {
-  ami           = var.ami
-  instance_type = "t2.micro"
-  key_name = "terraform-key"
-  security_groups = ["${aws_security_group.allow_rdp.name}"]
-}
-
-resource "aws_security_group" "allow_rdp" {
-  name = "allow_rdp"
-  description = "allow traffic"
-
-  ingress {
-    from_port = 3389
-    to_port = 3389
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
