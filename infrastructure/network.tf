669e8477cd57c61ef899d79e1206d4ae15904f7f
Sourav Kundu <skundu.dev@gmail.com>
2024-12-30 15:54:51 -0600

    added scaffolding code

---
+++ infrastructure/network.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source                  = "github.com/kunduso/terraform-aws-vpc?ref=v1.0.2"
+  region                  = var.region
+  enable_internet_gateway = true
+  vpc_cidr                = "11.22.32.0/24"
+  subnet_cidr_public      = ["11.22.32.0/27", "11.22.32.32/27", "11.22.32.64/27"]
+  subnet_cidr_private     = ["11.22.32.128/27", "11.22.32.160/27", "11.22.32.192/27"]
+  #CKV_TF_1: Ensure Terraform module sources use a commit hash
+  #checkov:skip=CKV_TF_1: This is a self hosted module where the version number is tagged rather than the commit hash.
+}
