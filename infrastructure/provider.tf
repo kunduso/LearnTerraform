7fa1366796e2c720d5ff4d94db1c67432c1fd13a
Sourav Kundu <skundu.dev@gmail.com>
2025-01-01 07:39:36 -0600

    added provider block to close #4

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -14,3 +14,13 @@ terraform {
   }
   required_version = "~> 1.2"
 }
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  default_tags {
+    tags = {
+      Source = "https://github.com/kunduso/app-two"
+    }
+  }
+}
