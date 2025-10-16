51217f81f23fdbeeaeebf9bd9297fdb7def1ddf8
Sourav Kundu <skundu.dev@gmail.com>
2025-10-16 11:02:16 -0500

    update resource reference

--- terraform-import-aws-resource/main.tf
+++ terraform-import-aws-resource/main.tf
@@ -1,12 +1,12 @@
 # Import block for existing VPC
 import {
   to = aws_vpc.imported_vpc
-  id = "vpc-09b1634a885c02134"
+  id = "vpc-070e2d49d3100f46d"
 }
 
 # VPC resource configuration to match the manually created VPC
 resource "aws_vpc" "imported_vpc" {
-  cidr_block           = "10.0.0.0/24"
+  cidr_block           = "12.25.15.0/25"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
