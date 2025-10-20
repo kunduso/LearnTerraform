3753374ceb8f10b9abf83aceab5d3e1085289026
Sourav Kundu <skundu.dev@gmail.com>
2025-10-19 21:38:42 -0500

    add public subnet resources and import blocks for existing subnets

--- terraform-import-aws-resource/main.tf
+++ terraform-import-aws-resource/main.tf
@@ -14,3 +14,35 @@ resource "aws_vpc" "imported_vpc" {
     Name = "web-tier"
   }
 }
+
+# Import blocks for public subnets
+import {
+  to = aws_subnet.web_tier_public_1
+  id = "subnet-0ea4fe97c34c804e6"
+}
+
+import {
+  to = aws_subnet.web_tier_public_2
+  id = "subnet-05b20b4d8660a2bd4"
+}
+
+# Public subnet resources
+resource "aws_subnet" "web_tier_public_1" {
+  vpc_id            = aws_vpc.imported_vpc.id
+  cidr_block        = "12.25.15.0/27"  # You'll need to verify this CIDR
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "web-tier-public-1"
+  }
+}
+
+resource "aws_subnet" "web_tier_public_2" {
+  vpc_id            = aws_vpc.imported_vpc.id
+  cidr_block        = "12.25.15.32/27"  # You'll need to verify this CIDR
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "web-tier-public-2"
+  }
+}
