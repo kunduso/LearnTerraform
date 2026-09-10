398bb70ce1521e1fec646c85b3332a68024ccdbd
Sourav Kundu <skundu.dev@gmail.com>
2026-09-09 22:09:01 -0500

    update key path

--- aws-iam-auto-pilot/infra/backend.tf
+++ aws-iam-auto-pilot/infra/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-remote-state-076680484948"
-    key          = "tf/aws-iam-auto-pilot/terraform.tfstate"
+    key          = "tf/aws-iam-auto-pilot/infra/terraform.tfstate"
     region       = "us-east-2"
     use_lockfile = true
     encrypt      = true
